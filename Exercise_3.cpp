7390a39fae50bd83fa65d86134879c5728e9069c
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-05-20 16:44:01 +0530

    Update Exercise_3.cpp

--- Exercise_3.cpp
+++ Exercise_3.cpp
@@ -15,12 +15,13 @@ a new node on the front of the list. */
 void push(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* nNode = new Node();
     /* 2. put in the data */  
-  
+    nNode->data = new_data;
     /* 3. Make next of new node as head */ 
-  
+    nNode->next = *head_ref;
     /* 4. move the head to point to the new node */
+    *head_ref = nNode;
 }  
   
 /* Given a node prev_node, insert a new node after the given  
@@ -28,14 +29,15 @@ prev_node */
 void insertAfter(Node* prev_node, int new_data)  
 {  
     /*1. check if the given prev_node is NULL */ 
-  
+    if(prev_node == nullptr) return;
     /* 2. allocate new node */ 
-  
+    Node* nNode = new Node();
     /* 3. put in the data */ 
-  
+    nNode->data = new_data;
     /* 4. Make next of new node as next of prev_node */
-  
+    nNode->next = prev_node->next;
     /* 5. move the next of prev_node as new_node */ 
+    prev_node->next = nNode;
 }  
   
 /* Given a reference (pointer to pointer) to the head  
@@ -43,19 +45,26 @@ of a list and an int, appends a new node at the end */
 void append(Node** head_ref, int new_data)  
 {  
     /* 1. allocate node */ 
-  
+    Node* nNode = new Node();
     /* 2. put in the data */ 
-  
+    nNode->data = new_data;
     /* 3. This new node is going to be  
     the last node, so make next of  
     it as NULL*/  
-  
+    nNode->next = NULL;
     /* 4. If the Linked List is empty, 
     then make the new node as head */
-  
+    if (*head_ref == nullptr) {
+        *head_ref = nNode;
+        return;
+    }
     /* 5. Else traverse till the last node */
-  
+    Node* curr = new Node();
+    while(curr->next !=nullptr){
+        curr=curr->next;
+    }
     /* 6. Change the next of last node */ 
+    curr->next = nNode;
 }  
   
 // This function prints contents of 
@@ -63,6 +72,11 @@ void append(Node** head_ref, int new_data)
 void printList(Node *node)  
 {  
     //Your code here
+    while (node != nullptr) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
 }  
   
 /* Driver code*/
